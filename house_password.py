c34c2216a4b43bf31b8f8371dfc3cf0833054bda
Vitaliy Litvinov <mad_smile@list.ru>
2013-10-23 14:10:24 +0300

    house password done, non uniquie elements - need to think and read some about pylists

--- house_password.py
+++ house_password.py
@@ -8,29 +8,29 @@ def checkio(data):
     has_lower_case = 0
     has_digit = 0
 
-    if (data.__len__() >= 10) :
-        more_than_ten_symbols = 1
-    else :
+    if (data.__len__() < 10) :
         more_than_ten_symbols = 0
-    lower_case_pattern = re.compile('[a-z]')
-    upper_case_pattern = re.compile('[A-Z]')
-    digit_pattern = re.compile('[0-9]')
-    if re.search (upper_case_pattern, data) :
-        has_upper_case = 1
     else :
-        has_upper_case = 0
-    if re.search ( lower_case_pattern, data) :
-         has_lower_case = 1
+        more_than_ten_symbols = 1
+        lower_case_pattern = re.compile('[a-z]')
+        upper_case_pattern = re.compile('[A-Z]')
+        digit_pattern = re.compile('[0-9]')
+        if re.search (upper_case_pattern, data) :
+            has_upper_case = 1
+        else :
+            has_upper_case = 0
+        if re.search ( lower_case_pattern, data) :
+             has_lower_case = 1
+        else :
+             has_lower_case = 0
+        if re.search ( digit_pattern, data) :
+             has_digit = 1
+        else :
+             has_digit = 0
+    if (more_than_ten_symbols & has_digit & has_lower_case & has_lower_case): # basically now we can tell where is the problem, I HATE ERROR MESSAGE "INCORRECT PASSWORD OR LOGIN OR CATPTCHA"
+            password_is_awesome = True
     else :
-         has_lower_case = 0
-    if re.search ( digit_pattern, data) :
-         has_digit = 1
-    else :
-         has_digit = 0
-    if (more_than_ten_symbols & has_digit & has_lower_case & has_lower_case):
-        password_is_awesome = True
-    else :
-        password_is_awesome = False
+            password_is_awesome = False
     return password_is_awesome
 #These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
